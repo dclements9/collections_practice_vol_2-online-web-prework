b7d268521aa28b5b6c05db625f9429395e51263f
Dylan Clements <dclements@protonmail.com>
2019-01-11 18:25:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
-# your code goes here
+def begins_with_r(array)
+  status = true
+    array.each do |word|
+      if word[0] == "r"
+        status = true
+      else
+        status = false
+      end
+    end
+  status
+end
+
+def contain_a(array)
+  new_array = []
+  array.select do |word|
+    if word.include?("a")
+      new_array << word
+    end
+  end
+  new_array
+end
+
+def first_wa(array)
+    array.find {|word| word[0] == "w" && word[1] == "a"}
+  end
+
+  def remove_non_strings(array)
+    new_array = []
+
+     array.each do |variable|
+       if variable.is_a? String
+         new_array << variable
+      end
+    end
+    new_array
+   end
+
+def count_elements (array)
+  
+end
